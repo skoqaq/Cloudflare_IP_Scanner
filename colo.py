f74f2ebf1e433a4bac119fa872c2fa544cd3695c
Sakaro <48171385+realSakaro@users.noreply.github.com>
2021-08-03 17:59:07 +0800

    Add files via upload
    colo-data的数据来自 https://www.cloudflare.com/ips-v4
    大概不久后会根据BGP再做一份

---
+++ colo.py
@@ -0,0 +1,34 @@
+#Collect from https://luotianyi.vc/3258.html
+import requests, threading, queue
+ 
+IPQueue = queue.Queue()
+_WORKER_THREAD_NUM = 3
+ 
+class Workers(threading.Thread):
+ 
+    def run(self):
+        while not IPQueue.empty():
+            ip = IPQueue.get()
+            try:
+                rsp = requests.head('http://' + ip, timeout=2).headers
+                print(ip, rsp['CF-RAY'])
+            except:
+                pass
+ 
+ 
+def main():
+    global IPQueue
+    with open('colo-data.txt', 'r') as f:
+        for ip in f.read().splitlines():
+            IPQueue.put(ip)
+ 
+    threads = []
+    for i in range(_WORKER_THREAD_NUM) :
+        thread = Workers()
+        thread.start()
+        threads.append(thread)
+    for thread in threads :
+        thread.join()
+ 
+if __name__ == '__main__':
+    main()
